20523051eef6151c94ee51440ab91bba60d31027
JorLo <jory.laurin@gmail.com>
2022-07-02 21:46:17 -0700

    adds sales db and page to admin section

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,24 @@
 class Sale < ApplicationRecord
+
+  #AR scope
+
+  # another way to do self.active
+  # scope :active, -> { where("sales.starts_on <= ? AND sales.ends_on >= ?", Date.current, Date.current) }
+
+  def self.active
+    where("sales.starts_on <= ? AND sales.ends_on >= ?", Date.current, Date.current)
+  end
+  
+  def finished?
+    ends_on < Date.current
+  end
+
+  def upcoming?
+    starts_on > Date.current
+  end
+  
+  def active?
+    !upcoming? && !finished?
+  end
+
 end
